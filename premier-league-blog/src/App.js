15d2ec1afd750cd76c8d81fb6e4c75a4b49dd322
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-11 14:16:05 -0500

    linked aston villa page

--- premier-league-blog/src/App.js
+++ premier-league-blog/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Header from './components/Header'
 import ArsenalPage from './pages/Arsenal Page'
+import AstonVillaPage from './pages/AstonVillaPage'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/arsenal" element={<ArsenalPage />} />
+            <Route path="/astonvilla" element={<AstonVillaPage />} />
           </Routes>
         </main>
       </div>
---
+++ premier-league-blog/src/pages/AstonVillaPage.js
@@ -0,0 +1,14 @@
+const AstonVillaPage = () => {
+  return (
+    <div className="astonvilla">
+      <h1>
+        <center>Aston Vila</center>
+      </h1>
+      <h3>
+        <center></center>
+      </h3>
+    </div>
+  )
+}
+
+export default AstonVillaPage
