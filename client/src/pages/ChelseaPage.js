0c0921cd80cce4eacf3ee80c29519b9b59f31b93
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-13 12:28:42 -0500

    added form to chelsea

--- client/src/pages/ChelseaPage.js
+++ client/src/pages/ChelseaPage.js
@@ -1,8 +1,10 @@
+import Form from '../components/Form'
 const ChelseaPage = () => {
   return (
     <div className="chelsea">
       <h1>
         <center>Chelsea</center>
+        <Form />
       </h1>
       <h3>
         <center></center>
