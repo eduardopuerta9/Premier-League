4ba6385db499363c48d542e94e557bb1a4cf2064
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-13 12:20:48 -0500

    added forms to aston villa and bournemouth

--- client/src/pages/AstonVillaPage.js
+++ client/src/pages/AstonVillaPage.js
@@ -1,8 +1,10 @@
+import Form from '../components/Form'
 const AstonVillaPage = () => {
   return (
     <div className="astonvilla">
       <h1>
         <center>Aston Vila</center>
+        <Form />
       </h1>
       <h3>
         <center></center>
--- client/src/pages/BournemouthPage.js
+++ client/src/pages/BournemouthPage.js
@@ -1,8 +1,10 @@
+import Form from '../components/Form'
 const BournemouthPage = () => {
   return (
     <div className="bournemouth">
       <h1>
         <center>Bournemouth</center>
+        <Form />
       </h1>
       <h3>
         <center></center>
