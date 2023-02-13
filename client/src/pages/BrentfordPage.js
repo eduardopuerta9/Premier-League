980659b65188045dbe5a69878796c553e42bc2d4
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-13 12:22:07 -0500

    added form to brentford

--- client/src/pages/BrentfordPage.js
+++ client/src/pages/BrentfordPage.js
@@ -1,8 +1,10 @@
+import Form from '../components/Form'
 const BrentfordPage = () => {
   return (
     <div className="brentford">
       <h1>
         <center>Brentford</center>
+        <Form />
       </h1>
       <h3>
         <center></center>
