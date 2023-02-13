f6756b00ce654717a0c4c84fccfca04f0958024c
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-13 12:27:17 -0500

    added form to brighton

--- client/src/pages/BrightonPage.js
+++ client/src/pages/BrightonPage.js
@@ -1,8 +1,10 @@
+import Form from '../components/Form'
 const BrightonPage = () => {
   return (
     <div className="brighton">
       <h1>
         <center>Brighton</center>
+        <Form />
       </h1>
       <h3>
         <center></center>
