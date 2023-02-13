58918e369690e1b3e26acf08d7db0419a2032da7
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-13 13:28:15 -0500

    added everton

--- client/src/pages/EvertonPage.js
+++ client/src/pages/EvertonPage.js
@@ -1,8 +1,18 @@
+import Form from '../components/Form'
 const EvertonPage = () => {
   return (
     <div className="eveton">
       <h1>
         <center>Everton</center>
+        <center>
+          {' '}
+          <img
+            center
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Everton_FC_logo.svg/1200px-Everton_FC_logo.svg.png"
+            height={80}
+          ></img>
+        </center>
+        <Form />
       </h1>
       <h3>
         <center></center>
