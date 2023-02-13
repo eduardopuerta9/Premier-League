5371e000a292bf24cf1462f08a72df027f3107b3
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-13 13:36:06 -0500

    added fulham form

--- client/src/pages/FulhamPage.js
+++ client/src/pages/FulhamPage.js
@@ -1,8 +1,18 @@
+import Form from '../components/Form'
 const FulhamPage = () => {
   return (
     <div className="fulham">
       <h1>
         <center>Fulham</center>
+        <center>
+          {' '}
+          <img
+            center
+            src="https://upload.wikimedia.org/wikipedia/en/e/eb/Fulham_FC_%28shield%29.svg"
+            height={80}
+          ></img>
+        </center>
+        <Form />
       </h1>
       <h3>
         <center></center>
