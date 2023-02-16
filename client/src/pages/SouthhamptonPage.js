c7aa86ba368c5df59101d18a1bb089794e738bba
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 16:25:55 -0500

    added info southampton

--- client/src/pages/SouthhamptonPage.js
+++ client/src/pages/SouthhamptonPage.js
@@ -8,7 +8,22 @@ const SouthhamptonPage = () => {
           <img
             center
             src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/FC_Southampton.svg/1200px-FC_Southampton.svg.png"
-            height={80}
+            height={120}
+          ></img>
+          <br></br>
+          <h3>Founded: 1865 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/Mv50AJb_ve0/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: St Mary Stadium</h3>
+          <h4>Location: Southampton, England</h4>
+          <h4>Capacity: 32,384</h4>
+          <img
+            center
+            src="https://tmssl.akamaized.net/images/foto/stadionnormal/st-marys-stadium-des-fc-southampton-1641314222-77989.jpg?lm=1641314236"
           ></img>
         </center>
       </h1>
