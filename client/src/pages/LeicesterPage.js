eff108f185d0b9c0d36aca3b5b6e60da21bcae0c
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 15:57:29 -0500

    added leicter info

--- client/src/pages/LeicesterPage.js
+++ client/src/pages/LeicesterPage.js
@@ -8,7 +8,23 @@ const LeicesterPage = () => {
           <img
             center
             src="https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Leicester_City_crest.svg/1200px-Leicester_City_crest.svg.png"
-            height={80}
+            height={120}
+          ></img>
+          <br></br>
+          <h3>Founded: 1884 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/8-hSHyNwUIQ/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: King Power Stadium</h3>
+          <h4>Location: Leicester, England</h4>
+          <h4>Capacity: 32,261</h4>
+          <img
+            center
+            src="https://i2-prod.stokesentinel.co.uk/incoming/article3792904.ece/ALTERNATES/s615/0_KP-stadium.jpg"
+            height={300}
           ></img>
         </center>
       </h1>
