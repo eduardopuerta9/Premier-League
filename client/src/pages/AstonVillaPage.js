f6381a305eeda19ff2981c8399da8b0abfb09a04
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 15:21:48 -0500

    added info to brighton

--- client/src/pages/AstonVillaPage.js
+++ client/src/pages/AstonVillaPage.js
@@ -6,7 +6,7 @@ const AstonVillaPage = () => {
         <center>
           {' '}
           <img
-            //center
+            center
             src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Aston_Villa_FC_crest_%282016%29.svg/1200px-Aston_Villa_FC_crest_%282016%29.svg.png"
             height={120}
           ></img>
@@ -14,7 +14,7 @@ const AstonVillaPage = () => {
           <h3>Founded: 1874 </h3>
           <h3>Current Squad:</h3>
           <img
-            //center
+            center
             src="https://s3.eu-west-1.amazonaws.com/gc-media-assets.gc.avfcservices.co.uk/99b3e4e0-3a7d-11ed-88a1-5b8fd5ef23af.jpg"
             height={240}
           ></img>
@@ -22,7 +22,7 @@ const AstonVillaPage = () => {
           <h4>Location: Birmingham, England</h4>
           <h4>Capacity: 42,682</h4>
           <img
-            //center
+            center
             src="https://upload.wikimedia.org/wikipedia/commons/0/01/Villa_Park.jpg"
             height={300}
           ></img>
--- client/src/pages/BrightonPage.js
+++ client/src/pages/BrightonPage.js
@@ -10,6 +10,22 @@ const BrightonPage = () => {
             src="https://upload.wikimedia.org/wikipedia/en/thumb/f/fd/Brighton_%26_Hove_Albion_logo.svg/640px-Brighton_%26_Hove_Albion_logo.svg.png"
             height={80}
           ></img>
+          <br></br>
+          <h3>Founded: 1901 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/yzdg1hdEQP0/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: American Express Community Stadium</h3>
+          <h4>Location: Brighton, England</h4>
+          <h4>Capacity: 30,750</h4>
+          <img
+            center
+            src="https://w0.peakpx.com/wallpaper/514/1014/HD-wallpaper-american-express-community-stadium-falmer-stadium-brighton-and-hove-fc-stadium-premier-league-england-brighton-and-hove-fc-falmer-brighton-east-sussex.jpg"
+            height={300}
+          ></img>
         </center>
       </h1>
       <h3>
