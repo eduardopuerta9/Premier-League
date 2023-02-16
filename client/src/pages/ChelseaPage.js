a4ea476b69540b91c64554dd97ef32871daa7a39
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 15:28:59 -0500

    added info to chelsea

--- client/src/pages/ChelseaPage.js
+++ client/src/pages/ChelseaPage.js
@@ -11,6 +11,22 @@ const ChelseaPage = () => {
             src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png"
             height={80}
           ></img>
+          <br></br>
+          <h3>Founded: 1905 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/1Egk28Ydih4/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: Stamford Bridge</h3>
+          <h4>Location: London, England</h4>
+          <h4>Capacity: 40,341</h4>
+          <img
+            center
+            src="https://c4.wallpaperflare.com/wallpaper/484/501/148/england-football-football-stadium-wallpaper-preview.jpg"
+            height={300}
+          ></img>
         </center>
       </h1>
       <h3>
