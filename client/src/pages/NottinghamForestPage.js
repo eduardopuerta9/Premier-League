2dce81d7837ad9ddc09772bf34f14eb547ffa5b5
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 16:21:06 -0500

    added info nottingham

--- client/src/pages/NottinghamForestPage.js
+++ client/src/pages/NottinghamForestPage.js
@@ -8,7 +8,22 @@ const NottinghamForestPage = () => {
           <img
             center
             src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/393.png"
-            height={80}
+            height={120}
+          ></img>
+          <br></br>
+          <h3>Founded: 1865 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/ejxvz5Yzgqw/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: City Ground</h3>
+          <h4>Location: Nottingham, England</h4>
+          <h4>Capacity:30,445</h4>
+          <img
+            center
+            src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B958/production/_121184474_gettyimages-656718056.jpg"
           ></img>
         </center>
       </h1>
