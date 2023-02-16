aea80cae140a75ff78c34c5e6d4aa1f7ab5f0b56
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 15:53:28 -0500

    added info to leeds

--- client/src/pages/LeedsPage.js
+++ client/src/pages/LeedsPage.js
@@ -8,7 +8,23 @@ const LeedsPage = () => {
           <img
             center
             src="https://upload.wikimedia.org/wikipedia/en/thumb/5/54/Leeds_United_F.C._logo.svg/1200px-Leeds_United_F.C._logo.svg.png"
-            height={80}
+            height={120}
+          ></img>
+          <br></br>
+          <h3>Founded: 1919 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/qqTvH2-Rh80/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: Elland Road</h3>
+          <h4>Location: Leeds, England</h4>
+          <h4>Capacity: 37,792</h4>
+          <img
+            center
+            src="https://www.alliedtelesis.com/sites/default/files/styles/large_hq/public/image/2022-08/leeds-united-3840_0.jpg?itok=LcYDy1WL"
+            height={300}
           ></img>
         </center>
       </h1>
