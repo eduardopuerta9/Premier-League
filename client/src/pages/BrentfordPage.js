3fb51073469764d6ccb1101c2a89673a8e3501b1
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 12:40:42 -0500

    added brentford info

--- client/src/pages/BrentfordPage.js
+++ client/src/pages/BrentfordPage.js
@@ -8,7 +8,23 @@ const BrentfordPage = () => {
           <img
             center
             src="https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/Brentford_FC_crest.svg/1200px-Brentford_FC_crest.svg.png"
-            height={80}
+            height={120}
+          ></img>
+          <br></br>
+          <h3>Founded: 1889 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://pbs.twimg.com/media/FdLVLRpXkAQj_5y?format=jpg&name=4096x4096"
+            height={240}
+          ></img>
+          <h3>Stadium: Brentford Community Stadium</h3>
+          <h4>Location: Brentford, England</h4>
+          <h4>Capacity: 17,250</h4>
+          <img
+            center
+            src="https://www.buckinghamgroup.co.uk/wp-content/uploads/brentford_1-701x501.jpg"
+            height={300}
           ></img>
         </center>
       </h1>
