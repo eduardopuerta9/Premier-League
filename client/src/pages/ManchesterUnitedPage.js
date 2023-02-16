d0676deefbf004447c6dccfd4cdeb4ba02a28733
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 16:12:45 -0500

    added united info

--- client/src/pages/ManchesterUnitedPage.js
+++ client/src/pages/ManchesterUnitedPage.js
@@ -8,7 +8,23 @@ const ManchesterUnitedPage = () => {
           <img
             center
             src="https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png"
-            height={80}
+            height={120}
+          ></img>
+          <br></br>
+          <h3>Founded: 1878 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/A90M8pNSVcg/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: Old Trafford</h3>
+          <h4>Location: Manchester, England</h4>
+          <h4>Capacity: 74,310</h4>
+          <img
+            center
+            src="https://www.foottheball.com/wp-content/uploads/2017/01/man-united-old-trafford.jpg"
+            height={300}
           ></img>
         </center>
       </h1>
