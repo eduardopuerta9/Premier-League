950d19006faa8b65f6c0e93593bd952080aed113
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-02-16 16:16:09 -0500

    added info newcastle

--- client/src/pages/NewcastlePage.js
+++ client/src/pages/NewcastlePage.js
@@ -8,7 +8,22 @@ const NewcastlePage = () => {
           <img
             center
             src="https://upload.wikimedia.org/wikipedia/en/5/56/Newcastle_United_Logo.svg"
-            height={80}
+            height={120}
+          ></img>
+          <br></br>
+          <h3>Founded: 1892 </h3>
+          <h3>Current Squad:</h3>
+          <img
+            center
+            src="https://i.ytimg.com/vi/xIEMnqk_wYw/maxresdefault.jpg"
+            height={240}
+          ></img>
+          <h3>Stadium: St James Park</h3>
+          <h4>Location: Newcastle, England</h4>
+          <h4>Capacity: 52,000</h4>
+          <img
+            center
+            src="https://www.daimani.com/dvh-media/newcastle-2021-MB1-stadium-DAIMANI-VIP-tickets.jpg?context=bWFzdGVyfGltYWdlc3wyMjIxMzR8aW1hZ2UvanBlZ3xoYTUvaDI3Lzg5NzU2NjUwNzAxMTAvbmV3Y2FzdGxlLTIwMjEtTUIxLXN0YWRpdW0tREFJTUFOSS1WSVAtdGlja2V0cy5qcGd8YWI3NmZlZGM0NDA2MzNlZTE1NTM0YmZjNTIxNWNlZjAzYjQ0Y2QxNTU4ODg2MWJhN2UzYjBlYTE2YzVhNDg2Nw"
           ></img>
         </center>
       </h1>
